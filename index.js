48e2c1c19f9804b8fe5bfeac00ca744b46656718
kareemAlqubati <salamkareem792@gmail.com>
2024-08-22 17:49:55 +0300

    Complete

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (a,b) => a/b;
+const square = (x) => x*x;
+const add = (a,b) => a+b;
+
+const  result = divide(2000,100);
+const result2 = square(2);
+const result3 = add(3,4);
+
+console.log(result);
+console.log(result2);
+console.log(result3);
